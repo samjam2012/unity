df17746424fa157c7e60e8d87c31d5ccc271118e
Sam Jamal <sammy@ultra.me>
2020-05-08 20:37:44 -0700

    merge unity-api into unity

---
+++ unity-api/src/server.ts
@@ -0,0 +1,39 @@
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as middleware from "./middleware";
+
+import {
+  createUser,
+  getUsers,
+  getUserByAuthId,
+  updateUser,
+  deleteUser
+} from "./queries";
+
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+// app.use(middleware);
+
+app.get("/", (req: any, res: any) => {
+  res.json({ info: "hi" });
+});
+
+app.post("/users", createUser);
+app.get("/users", getUsers);
+app.get("/users/:authId", getUserByAuthId);
+app.put("/users/:authId", updateUser);
+app.delete("/users/:authId", deleteUser);
+
+const port = process.env.PORT || 8090;
+app.listen(port, () => {
+  console.log(`Running on port ${port}...`);
+});
---
+++ unity-api/ts-modules.d.ts
@@ -0,0 +1,9 @@
+// server
+declare module "body-parser";
+declare module "cors";
+declare module "express";
+
+// packages
+declare module "dotenv";
+declare module "lodash";
+declare module "pg";
